d43e442f1f628f9ce7c76e690486fde942c315ef
GuilhermeEDS <102765171+GuilhermeEDS@users.noreply.github.com>
2022-03-31 18:22:19 -0300

    Resolução Negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,14 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
+    int x;
+    int y=0;
+    for (int i=0; i<SIZE; i++){
+        cin >> x;
+        if(x<0){
+            y++;
+        }
+    }
+    cout << y;
     return 0;
 }
